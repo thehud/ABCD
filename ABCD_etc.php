8cdab7bfa4f43ba37063563826188c4e0e676228
thehud <hud@thehud.com>
2016-03-18 18:33:46 -0700

    Early Code Upload

---
+++ ABCD_etc.php
@@ -0,0 +1,7 @@
+<?php
+define('ABCD_DB','abcd');
+define('ABCD_HOST','localhost');
+define('ABCD_USER','apex');
+define('ABCD_PASS','study22');
+define('ABCD_DB','abcd');
+define('ROOT_URL','http://localhost:8888/');
